7f0e7a77258169c5acbc0e4bc2159c34844e9722
hs673 <90913830+hs673@users.noreply.github.com>
2022-11-05 16:58:37 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <title>NJIT Schedule Generator</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+    <header>
+        <h1>NJIT Schedule Generator</h1>
+    </header>
+</body>
+</html>
