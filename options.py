46aabdc1cf4aff81092984c7062774b0c2ae3a35
PCakes1 <patrickkrawczyk1@gmail.com>
2022-11-06 05:32:47 -0500

    latest options

--- options.py
+++ options.py
@@ -4,12 +4,10 @@ from pprint import pprint
 
 #pass student classes thru command line
 #convert requested courses into a map -> CType:Cnumber
-courseRequests = {}
-for arg in sys.argv:
-    if arg=='options.py':
-        continue
+courseRequests = []
+for arg in sys.argv[1:]:
     c = arg.split(",")
-    courseRequests[c[0]] = c[1]
+    courseRequests.append((c[0],c[1]))
 
 mydb = mysql.connector.connect(
   host="sql.njit.edu",
@@ -23,11 +21,14 @@ mycursor = mydb.cursor()
 
 #prepare sql for getting all requested courses
 courses = []
-sql = "SELECT * FROM Course as C, Section as S, Meet as M WHERE ("
-for cat in courseRequests.keys():
-    sql += "C.CourseCat=\"" + cat + "\" AND C.Number=\"" + courseRequests[cat] + "\" AND C.CourseId=S.CourseId AND S.CRN=M.CRN)"
+for cat, num in courseRequests:
+    sql = "SELECT * FROM Course as C, Section as S, Meet as M WHERE ("
+    sql += "C.CourseCat=\"" + cat + "\" AND C.Number=\"" + num + "\" AND C.CourseId=S.CourseId AND S.CRN=M.CRN)"
     mycursor.execute(sql)
     courses.append(mycursor.fetchall())
 
 pprint(courses)
-  
+
+#make combos
+for i in len(courses):
+    
