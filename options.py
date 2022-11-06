59c007a9d14a5b7511c64e9b7ac7a0d53db47483
DaleNaci <dtn22@njit.edu>
2022-11-06 07:41:04 -0500

    Working new print

--- options.py
+++ options.py
@@ -3,8 +3,20 @@ import sys
 from pprint import pprint
 from data.helper_functions import check_conflict
 
-#pass student classes thru command line
-#convert requested courses into a map -> CType:Cnumber
+import itertools
+
+
+class Section:
+    def __init__(self, course_category, course_number, course_name, crn, professor_name):
+        self.course_category = course_category
+        self.course_number = course_number
+        self.course_name = course_name
+        self.crn = crn
+        self.professor_name = professor_name
+        self.start = None
+        self.end = None
+
+
 courseRequests = []
 for arg in sys.argv[1:]:
     c = arg.split(",")
@@ -23,33 +35,18 @@ mycursor = mydb.cursor()
 #prepare sql for getting all requested courses
 courses = []
 for cat, num in courseRequests:
-    sql = "SELECT * FROM Course as C, Section as S, Meet as M WHERE ("
-    sql += "C.CourseCat=\"" + cat + "\" AND C.Number=\"" + num + "\" AND C.CourseId=S.CourseId AND S.CRN=M.CRN)"
+    sql = "SELECT C.CourseCat, C.Number, C.Name, S.CRN, S.ProfessorName FROM Course as C, Section as S WHERE ("
+    sql += "C.CourseCat=\"" + cat + "\" AND C.Number=\"" + num + "\" AND C.CourseId=S.CourseId)"
     mycursor.execute(sql)
-    courses.append(mycursor.fetchall())
+    # courses.append(mycursor.fetchall())
+    sections = []
 
-#info for each course
-#[(0:CourseId, 1:CourseCat, 2:CourseNum, 3:CourseName, 4:CRN, 5:CourseId, 
-# 6:ProfessorName, 7:MeetId, 8:CRN, 9:Day, 10:Time)]
+    for section_info in mycursor.fetchall():
+        sections.append(Section(section_info[0], section_info[1], section_info[2], section_info[3], section_info[4]))
 
-#get number of sections for each course
-for course in courses:
-    crns = []
-    #add section number attribute as first element
-    course.insert(0, 0)
-    for possibleSection in course[1:]:
-        if course[4] not in crns:
-            crns.append()
+    courses.append(sections)
 
-#make combos
-combos = []
-#get total combos
-total = 0
-for course in courses:
-    total *= 
-for course in courses:
-    for section in course:
-        for otherSections in combos:
+x = list(itertools.product(*courses))
 
-            if check_conflict(section[9], section[10], ):
-                combos.append(section)
+for t in x:
+    print(t[0].crn, t[1].crn, t[2].crn)
