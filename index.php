3a7f75d97129a8360f303f1e1a573debc1ea95f8
hs673 <90913830+hs673@users.noreply.github.com>
2022-11-06 08:13:28 -0500

    Update index.php
    took out unnecessary comments and space

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <html lang="en">
 <head>
     <meta charset="UTF-8" />
@@ -19,7 +18,7 @@
                 <button class="btn btn-outline-secondary">Add Course &nbsp; 
                     <span>+ </span>
                 </button>
-                <p><br>Course Category &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Course Number</p>
+                <p><br>Course Category &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Course Number</p>
                 <div id="custom-margin"><input type="course-category" name="course1"><input type="course-number" name="num1"></div>
             </div>
             <input id="x" type="hidden" name="x" value="0" />
@@ -54,66 +53,5 @@
         });
         </script>
     </main>
-    <!--
-    <main>
-        <form>
-            <div class="form-group" >
-                <label for="number-of-classes">Number of Classes</label>
-                <select class="form-control text-center" id="number-of-classes">
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                  <option>6</option>
-                </select>
-              </div>
-            <div class="form-group">
-                <label for="class-category">Class Category:</label>
-                <input type="class-category" class="form-control" id="class-category">
-            </div>
-            <div class="form-group">
-                <label for="class-number">Class Number:</label>
-                <input type="class-number" class="form-control" id="class-number">
-
-                <button class="add_form_field">Add New Field &nbsp; 
-                  <span style="font-size:16px; font-weight:bold;">+ </span>
-                </button>
-            </div>
-
-            <button type="submit" class="btn btn-outline-success">Generate</button>
-        </form>
-    
-    </main>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    <script>    
-        $(document).ready(function() {
-            var max_fields = 6;
-            var wrapper = $("form-group");
-            var add_button = $(".add_form_field");
-
-            var x = 1;
-            $(add_button).click(function(e) {
-                e.preventDefault();
-                if (x < max_fields) {
-                    x++;
-                    $(wrapper).append('<div class="form-group"><label for="class-category">Class Category:</label><input type="class-category" class="form-control" id="class-category"></div>');
-                    $(wrapper).append('<div class="form-group"><label for="class-number">Class Number:</label><input type="class-number" class="form-control" id="class-number"></div>');
-             //add input box
-                } else {
-                    alert('You Reached the limits')
-                }
-            });
-
-            $(wrapper).on("click", ".delete", function(e) {
-                e.preventDefault();
-                $(this).parent('div').remove();
-                x--;
-            })
-        });
-    </script>
-    -->
-
 </body>
 </html>
