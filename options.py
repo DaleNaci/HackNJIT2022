7ed9fd5257d237bb4fd1ecdfa5b9c72238c2b5c8
PCakes1 <patrickkrawczyk1@gmail.com>
2022-11-06 06:31:54 -0500

    latest options

--- options.py
+++ options.py
@@ -1,6 +1,7 @@
 import mysql.connector
 import sys
 from pprint import pprint
+from data.helper_functions import check_conflict
 
 #pass student classes thru command line
 #convert requested courses into a map -> CType:Cnumber
@@ -27,8 +28,28 @@ for cat, num in courseRequests:
     mycursor.execute(sql)
     courses.append(mycursor.fetchall())
 
-pprint(courses)
+#info for each course
+#[(0:CourseId, 1:CourseCat, 2:CourseNum, 3:CourseName, 4:CRN, 5:CourseId, 
+# 6:ProfessorName, 7:MeetId, 8:CRN, 9:Day, 10:Time)]
+
+#get number of sections for each course
+for course in courses:
+    crns = []
+    #add section number attribute as first element
+    course.insert(0, 0)
+    for possibleSection in course[1:]:
+        if course[4] not in crns:
+            crns.append()
 
 #make combos
-for i in len(courses):
-    
+combos = []
+#get total combos
+total = 0
+for course in courses:
+    total *= 
+for course in courses:
+    for section in course:
+        for otherSections in combos:
+
+            if check_conflict(section[9], section[10], ):
+                combos.append(section)
