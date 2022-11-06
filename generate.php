79e073fc6045225689738df6d4d64e1937f3a209
PCakes1 <patrickkrawczyk1@gmail.com>
2022-11-06 11:48:54 -0500

    Updated generate.php

--- generate.php
+++ generate.php
@@ -1,12 +1,12 @@
 <?php
 #add first 5 parameters
-$args = $_POST["priority1"] . " " . $_POST["priority2"] . " " . $_POST["priority3"] . " " . $_POST["priority4"] . " " . $_POST["priority5"] = " ";
+$args = $_POST["priority1"] . " " . $_POST["priority2"] . " " . $_POST["priority3"] . " " . $_POST["priority4"] . " " . $_POST["priority5"] . " ";
 for($x = (int)$_POST["x"]; $x>0; $x-=1){
     $args = $args . $_POST["course{$x}"] . "," . $_POST["num{$x}"] . " ";
 }
-exec("cd data");
-exec("python data.options.py {$args}", $out);
-var_dump($out);
+
+$out = shell_exec("/afs/cad/linux/anaconda3.8/anaconda/bin/python data/options.py {$args}");
+var_export($out);
 /*foreach ($out as $line){
     echo $line;
 }*/
