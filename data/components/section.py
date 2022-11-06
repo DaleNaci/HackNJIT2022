aab4b8e101d62bbd8c39302c7eb95efaa083113f
DaleNaci <dtn22@njit.edu>
2022-11-06 07:51:55 -0500

    Refactored options.py

---
+++ data/components/section.py
@@ -0,0 +1,9 @@
+class Section:
+    def __init__(self, course_category, course_number, course_name, crn, professor_name):
+        self.course_category = course_category
+        self.course_number = course_number
+        self.course_name = course_name
+        self.crn = crn
+        self.professor_name = professor_name
+        self.start = None
+        self.end = None
--- options.py
+++ options.py
@@ -1,52 +1,53 @@
-import mysql.connector
 import sys
-from pprint import pprint
-from data.helper_functions import check_conflict
-
 import itertools
 
+from pprint import pprint
+import mysql.connector
 
-class Section:
-    def __init__(self, course_category, course_number, course_name, crn, professor_name):
-        self.course_category = course_category
-        self.course_number = course_number
-        self.course_name = course_name
-        self.crn = crn
-        self.professor_name = professor_name
-        self.start = None
-        self.end = None
+from data.helper_functions import check_conflict
+from data.components.section import Section
 
 
-courseRequests = []
-for arg in sys.argv[1:]:
-    c = arg.split(",")
-    courseRequests.append((c[0],c[1]))
+def main():
+    x = get_combination()
 
-mydb = mysql.connector.connect(
-  host="sql.njit.edu",
-  user="pk577",
-  password="Data147159!!",
-  port="3306",
-  database="pk577"
-)
+    for t in x:
+        print(t[0].crn, t[1].crn, t[2].crn)
 
-mycursor = mydb.cursor()
 
-#prepare sql for getting all requested courses
-courses = []
-for cat, num in courseRequests:
-    sql = "SELECT C.CourseCat, C.Number, C.Name, S.CRN, S.ProfessorName FROM Course as C, Section as S WHERE ("
-    sql += "C.CourseCat=\"" + cat + "\" AND C.Number=\"" + num + "\" AND C.CourseId=S.CourseId)"
-    mycursor.execute(sql)
-    # courses.append(mycursor.fetchall())
-    sections = []
+def get_combination():
+    courseRequests = []
+    for arg in sys.argv[1:]:
+        c = arg.split(",")
+        courseRequests.append((c[0],c[1]))
 
-    for section_info in mycursor.fetchall():
-        sections.append(Section(section_info[0], section_info[1], section_info[2], section_info[3], section_info[4]))
+    mydb = mysql.connector.connect(
+    host="sql.njit.edu",
+    user="pk577",
+    password="Data147159!!",
+    port="3306",
+    database="pk577"
+    )
 
-    courses.append(sections)
+    mycursor = mydb.cursor()
 
-x = list(itertools.product(*courses))
+    #prepare sql for getting all requested courses
+    courses = []
+    for cat, num in courseRequests:
+        sql = "SELECT C.CourseCat, C.Number, C.Name, S.CRN, S.ProfessorName FROM Course as C, Section as S WHERE ("
+        sql += "C.CourseCat=\"" + cat + "\" AND C.Number=\"" + num + "\" AND C.CourseId=S.CourseId)"
+        mycursor.execute(sql)
+        # courses.append(mycursor.fetchall())
+        sections = []
 
-for t in x:
-    print(t[0].crn, t[1].crn, t[2].crn)
+        for section_info in mycursor.fetchall():
+            sections.append(Section(section_info[0], section_info[1], section_info[2], section_info[3], section_info[4]))
+
+        courses.append(sections)
+
+    return list(itertools.product(*courses))
+
+
+
+if __name__ == "__main__":
+    main()
